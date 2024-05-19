0ef2059a014172e0944528fc4c4cdbe13e2ecd48
Matae300 <150396856+Matae300@users.noreply.github.com>
2024-05-19 18:12:29 -0400

    made changes to main jsx and app jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,23 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+
 import App from './App.jsx'
-import './index.css'
+import Home from './pages/Home.jsx'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <h1 className='display-2'>Problem displaying page!</h1>,
+    children: [
+      {
+        index: true,
+        element: <Home />
+      }, 
+    ]
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <RouterProvider router={router} />
 )
